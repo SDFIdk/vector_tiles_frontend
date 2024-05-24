4967f6fd3d21f81e6719cf88b1c7c85d7c1edb5a
Johan Lund Neergaard <johanlundn@gmail.com>
2024-05-24 10:23:42 +0200

    add LABS_ prefix to stylefiles

--- src/constants.js
+++ src/constants.js
@@ -2,21 +2,21 @@ export const STYLE_FILES = [
   {
     title: 'Klassisk', // The title displayed in the styling selector
     img: 'img/klassisk.png', // File location of the image to be diplayed in the styling selector
-    style: 'styles/klassisk.json' // File location of the style file
+    style: 'styles/LABS_klassisk.json' // File location of the style file
   },
   {
     title: 'Dæmpet',
     img: 'img/daempet.png',
-    style: 'styles/daempet.json'
+    style: 'styles/LABS_daempet.json'
   },
   {
     title: 'Grå',
     img: 'img/graa.png',
-    style: 'styles/graa.json'
+    style: 'styles/LABS_graa.json'
   },
   {
     title: 'Mørkt',
     img: 'img/moerkt.png',
-    style: 'styles/moerkt.json'
+    style: 'styles/LABS_moerkt.json'
   }
 ]

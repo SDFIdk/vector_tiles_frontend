595f9cccbb42f460b24489959324bc8dc025d7c2
mikarsdfi <168088613+mikarsdfi@users.noreply.github.com>
2024-09-27 11:21:50 +0200

    Update constants.js
    updated style file names - skaermkort

--- src/constants.js
+++ src/constants.js
@@ -2,21 +2,21 @@ export const STYLE_FILES = [
   {
     title: 'Klassisk', // The title displayed in the styling selector
     img: 'img/klassisk.png', // File location of the image to be diplayed in the styling selector
-    style: 'styles/official/LABS_klassisk.json' // File location of the style file
+    style: 'styles/official/skaermkort_klassisk.json' // File location of the style file
   },
   {
     title: 'Dæmpet',
     img: 'img/daempet.png',
-    style: 'styles/official/LABS_daempet.json'
+    style: 'styles/official/skaermkort_daempet.json'
   },
   {
     title: 'Grå',
     img: 'img/graa.png',
-    style: 'styles/official/LABS_graa.json'
+    style: 'styles/official/skaermkort_graa.json'
   },
   {
     title: 'Mørkt',
     img: 'img/moerkt.png',
-    style: 'styles/official/LABS_moerkt.json'
+    style: 'styles/official/skaermkort_moerkt.json'
   }
 ]
